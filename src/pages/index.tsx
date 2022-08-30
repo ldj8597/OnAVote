b89697c50faa7074fba1072147f0939917f74d0b
larkascending <ldj8597@gmail.com>
2022-08-30 21:58:29 +0900

    trpc working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,20 @@
 import { PollQuestion } from "@prisma/client";
 import type { GetServerSideProps, NextPage } from "next";
-import Head from "next/head";
-import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
-interface Props {
-  questions: PollQuestion[];
-}
-
-const Home: NextPage<Props> = ({ questions }) => {
-  const { data, isLoading } = trpc.useQuery(["hello"]);
+const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["getAllQuestions"]);
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div>{data.greeting}</div>;
-};
+  console.log(data);
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-  return {
-    props: {
-      questions: JSON.parse(JSON.stringify(questions)),
-    },
-  };
+  return (
+    <div>
+      {data.map((q) => (
+        <p key={q.id}>{q.question}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
