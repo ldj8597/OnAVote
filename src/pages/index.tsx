390b979eebb6a8fcd02f891c11fd8233d77afe72
larkascending <ldj8597@gmail.com>
2022-08-31 00:15:17 +0900

    ability to add questions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import { PollQuestion } from "@prisma/client";
 import type { NextPage } from "next";
+import { useRef } from "react";
 import { trpc } from "../utils/trpc";
 
 const QuestionCreator: React.FC = () => {
-  const { mutate, isLoading } = trpc.useMutation("questions.create");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const client = trpc.useContext();
+  const { mutate, isLoading } = trpc.useMutation("questions.create", {
+    onSuccess: (data) => {
+      client.invalidateQueries("questions.all");
+      if (!inputRef.current) return;
+      inputRef.current.value = "";
+    },
+  });
 
   return (
     <div>
-      <h2 className="text-2xl font-bold">Create Poll</h2>
+      <h2 className="text-2xl font-bold mb-5">Create Poll</h2>
       <form
-        className=""
+        className="max-w-xl flex flex-col gap-5"
         onSubmit={(e) => {
           e.preventDefault();
 
@@ -19,11 +28,28 @@ const QuestionCreator: React.FC = () => {
           mutate({
             question: $question.value,
           });
+          // console.log($question.value);
+          // $question.value = "";
         }}
       >
-        <label htmlFor="question">Question:</label>
-        <input id="question" name="question" type="text" />
-        <input type="submit" />
+        <label className="flex flex-col gap-1">
+          <span>Question</span>
+          <input
+            ref={inputRef}
+            className="border rounded py-2 px-3"
+            id="question"
+            name="question"
+            type="text"
+            disabled={isLoading}
+          />
+        </label>
+        <button
+          className="py-2 bg-slate-500 text-white rounded hover:bg-slate-400"
+          type="submit"
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : "Submit"}
+        </button>
       </form>
     </div>
   );
@@ -34,10 +60,10 @@ const Home: NextPage = () => {
   if (isLoading || !data) return <div>Loading...</div>;
 
   return (
-    <div className="max-w-2xl mx-auto px-5 py-5 flex flex-col gap-10">
+    <div className="max-w-2xl mx-auto px-5 py-5 flex flex-col gap-14">
       <div>
-        <h2 className="text-2xl font-bold">Polls</h2>
-        <ul>
+        <h2 className="text-2xl font-bold mb-5">Active Polls</h2>
+        <ul className="space-y-3">
           {data.map((q) => (
             <li key={q.id}>{q.question}</li>
           ))}
