8700b191ce6beede6cd6f769ddf6f7c7cae1fd4f
larkascending <ldj8597@gmail.com>
2022-08-30 21:27:57 +0900

    tRPC working now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,17 @@ import { PollQuestion } from "@prisma/client";
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { prisma } from "../db/client";
+import { trpc } from "../utils/trpc";
 
 interface Props {
   questions: PollQuestion[];
 }
 
 const Home: NextPage<Props> = ({ questions }) => {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <main>
-        <h1 className="text-2xl font-bold">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <ul>
-          {questions.map((q) => (
-            <li key={q.id}>{q.question}</li>
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
+  const { data, isLoading } = trpc.useQuery(["hello"]);
+  if (isLoading || !data) return <div>Loading...</div>;
+
+  return <div>{data.greeting}</div>;
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
