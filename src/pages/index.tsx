2a3d4761c2b6ede0bddfd5c935e07cf8f644fcf3
larkascending <ldj8597@gmail.com>
2022-08-30 20:23:02 +0900

    prisma added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from "next";
+import { PollQuestion } from "@prisma/client";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import { prisma } from "../db/client";
 
-const Home: NextPage = () => {
+interface Props {
+  questions: PollQuestion[];
+}
+
+const Home: NextPage<Props> = ({ questions }) => {
   return (
     <div>
       <Head>
@@ -11,9 +17,23 @@ const Home: NextPage = () => {
         <h1 className="text-2xl font-bold">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <ul>
+          {questions.map((q) => (
+            <li key={q.id}>{q.question}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const questions = await prisma.pollQuestion.findMany();
+  return {
+    props: {
+      questions: JSON.parse(JSON.stringify(questions)),
+    },
+  };
 };
 
 export default Home;
