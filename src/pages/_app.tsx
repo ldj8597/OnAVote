a83fb8ea71108dcb52e6446023087a6042256c22
larkascending <ldj8597@gmail.com>
2022-08-31 10:10:09 +0900

    fix vercel url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import { withTRPC } from "@trpc/next";
 import { AppType } from "next/dist/shared/lib/utils";
-// import { AppRouter } from "./api/trpc/[trpc]";
 import { AppRouter } from "../server/routers/_app";
 import superjson from "superjson";
 
@@ -15,8 +14,8 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return {
