ec85bd179bb2cc3460a4e8967f7539a8a7fb6583
larkascending <ldj8597@gmail.com>
2022-09-01 22:49:33 +0900

    trying to fix cors issue. install cors package and delete headers() in next.config.js

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,36 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/routers/_app";
 import { createContext } from "../../../server/context";
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+const cors = Cors();
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export function withCors(handler: NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    await runMiddleware(req, res, cors);
+
+    return await handler(req, res);
+  };
+}
 
 // export API handler
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext,
-  // createContext: () => null,
-});
+export default withCors(
+  trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext,
+    // createContext: () => null,
+  })
+);
