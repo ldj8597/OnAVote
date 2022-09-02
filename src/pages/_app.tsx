23e6eb4cbd6274000decfd365d0c9fbc35096e68
larkascending <ldj8597@gmail.com>
2022-09-03 01:43:29 +0900

    change headers function for ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,15 +38,20 @@ export default withTRPC<AppRouter>({
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
-      headers: () => {
-        if (ctx?.req) {
-          // on ssr, forward client's headers to the server
-          return {
-            ...ctx.req.headers,
-            "x-ssr": "1",
-          };
-        }
-        return {};
+      // headers: () => {
+      //   if (ctx?.req) {
+      //     // on ssr, forward client's headers to the server
+      //     return {
+      //       ...ctx.req.headers,
+      //       "x-ssr": "1",
+      //     };
+      //   }
+      //   return {};
+      // },
+      headers() {
+        return {
+          cookie: ctx?.req?.headers.cookie,
+        };
       },
     };
   },
