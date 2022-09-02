5c69b87341a1084b4f52ca53ab50a6d7fc692a6c
larkascending <ldj8597@gmail.com>
2022-09-03 01:28:35 +0900

    forward the headers from the browser to enable ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,10 +38,20 @@ export default withTRPC<AppRouter>({
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      headers: () => {
+        if (ctx?.req) {
+          // on ssr, forward client's headers to the server
+          return {
+            ...ctx.req.headers,
+            "x-ssr": "1",
+          };
+        }
+        return {};
+      },
     };
   },
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: false,
+  ssr: true,
 })(MyApp);
